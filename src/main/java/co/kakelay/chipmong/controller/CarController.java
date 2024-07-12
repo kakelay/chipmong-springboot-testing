d4b89c3b185837ee2de3fc826e4f5d713104f0f2
kakelay <kakelay18052002@gmail.com>
2024-07-12 11:10:17 +0700

    update code     If Bye, then close your program

--- src/main/java/co/kakelay/chipmong/controller/CarController.java
+++ src/main/java/co/kakelay/chipmong/controller/CarController.java
@@ -2,6 +2,8 @@ package co.kakelay.chipmong.controller;
 
 import co.kakelay.chipmong.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,8 +13,23 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+    @Autowired
+    private ApplicationContext context;
+
     @GetMapping("/ask")
     public String askCarName(@RequestParam String name) {
+        if (name.equalsIgnoreCase("Bye")) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(500);
+                    // Wait for 500 millis before shutting down
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                SpringApplication.exit(context, () -> 0);
+            }).start();
+            return "Goodbye! Your application will shut down shortly.";
+        }
         return carService.askCarName(name);
     }
 
